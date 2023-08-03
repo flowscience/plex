4dd2617b3be29e059c3624e9d9eb5cf8f4a1b3f1
Elijah Spina, PhD <ejspina@gmail.com>
2023-08-03 09:45:35 -0700

    Update PlexHypercertMinter.sol

--- contracts/contracts/PlexHypercertMinter.sol
+++ contracts/contracts/PlexHypercertMinter.sol
@@ -1,25 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Import the Hypercerts interface and PausableUpgradeable for pausability.
-import "./IHypercertToken.sol";
-import "oz-upgradeable/security/PausableUpgradeable.sol";
+npm install @openzeppelin/contracts
 
-// Import the PLEX token contract.
+// Import contracts
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/governance/MultisigWallet.sol";
+import "./IHypercertToken.sol";
 import "./ProofOfScience.sol";
 
-contract PlexHypercertMinter is IHypercertToken, ProofOfScience, PausableUpgradeable {
-    // Functions and variables from the ProofOfScience contract are available here.
+// Define contract
+contract PlexHypercertMinter is IHypercertToken, ProofOfScience, Pausable, AccessControl {
+    mapping(uint256 => uint256) private plexTokenIdToHypercertId;
+    mapping(address => bool) private owners;
+    address[] private ownerList;
+    MultiSigWallet private multiSigWallet;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    // Implementation of the IHypercertToken interface.
+    constructor(address[] memory _owners, uint8 _requiredConfirmations) {
+        multiSigWallet = new MultiSigWallet(_owners, _requiredConfirmations);
+        for (uint i=0; i<_owners.length; i++) {
+            owners[_owners[i]] = true;
+            ownerList.push(_owners[i]);
+        }
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
 
     function mintClaim(
         address account,
         uint256 units,
         string memory uri,
         TransferRestrictions restrictions
-    ) external override {
-        // Implement the function according to the requirements.
+    ) external override whenNotPaused {
+        require(account != address(0), "Cannot mint to zero address");
+        require(units > 0, "Units must be greater than zero");
+        _mint(account, units, uri, restrictions);
+        emit ClaimMinted(account, units, uri);
     }
 
     function mintClaimWithFractions(
@@ -28,60 +45,68 @@ contract PlexHypercertMinter is IHypercertToken, ProofOfScience, PausableUpgrade
         uint256[] memory fractions,
         string memory uri,
         TransferRestrictions restrictions
-    ) external override {
-        // Implement the function according to the requirements.
+    ) external override whenNotPaused {
+        // Validation and minting logic goes here.
+        emit ClaimMintedWithFractions(account, units, fractions, uri);
     }
 
-    function splitFraction(address account, uint256 tokenID, uint256[] memory _values) external override {
-        // Implement the function according to the requirements.
+    function splitFraction(address account, uint256 tokenID, uint256[] memory _values) external override whenNotPaused {
+        // Splitting logic goes here.
+        emit FractionSplit(account, tokenID, _values);
     }
 
-    function mergeFractions(address account, uint256[] memory tokenIDs) external override {
-        // Implement the function according to the requirements.
+    function mergeFractions(address account, uint256[] memory tokenIDs) external override whenNotPaused {
+        // Merging logic goes here.
+        emit FractionsMerged(account, tokenIDs);
     }
 
-    function burnFraction(address account, uint256 tokenID) external override {
-        // Implement the function according to the requirements.
+    function burnFraction(address account, uint256 tokenID) external override whenNotPaused {
+        // Burning logic goes here.
+        emit FractionBurned(account, tokenID);
     }
 
     function unitsOf(uint256 tokenID) external view override returns (uint256 units) {
-        // Implement the function according to the requirements.
+        // Returns the total units of the token.
     }
 
     function unitsOf(address account, uint256 tokenID) external view override returns (uint256 units) {
-        // Implement the function according to the requirements.
+        // Returns the total units of the token for a specific account.
     }
 
     function uri(uint256 tokenID) external view override(ProofOfScience, IHypercertToken) returns (string memory metadata) {
-        // Implement the function according to the requirements.
+        // Returns the metadata URI of the token.
     }
 
-    // PlexCert function
-    function plexCert(uint256 plexTokenId, string memory uri) public {
-    // The address that will own the new Hypercert.
-    address account = msg.sender;
+    function plexCert(uint256 plexTokenId, string memory uri) public whenNotPaused {
+        // The address that will own the new Hypercert.
+        address account = msg.sender;
 
-    // The total number of units that the Hypercert represents.
-    uint256 units = 1;
+        // The total number of units that the Hypercert represents.
+        uint256 units = 1;
 
-    // The restrictions on the transferability of the Hypercert.
-    IHypercertToken.TransferRestrictions restrictions = IHypercertToken.TransferRestrictions.AllowAll;
+        // The restrictions on the transferability of the Hypercert.
+        IHypercertToken.TransferRestrictions restrictions = IHypercertToken.TransferRestrictions.AllowAll;
 
-    // Mint a new Hypercert that represents the same scientific proof as the PLEX token.
-    hypercerts.mintClaim(account, units, uri, restrictions);
+        // Mint a new Hypercert that represents the same scientific proof as the PLEX token.
+        mintClaim(account, units, uri, restrictions);
 
-    // Retrieve the ID of the new Hypercert.
-    uint256 hypercertId = hypercerts.totalSupply();
+        // Retrieve the ID of the new Hypercert.
+        uint256 hypercertId = this.totalSupply();
 
-    // Store the PLEX token ID in a mapping, with the Hypercert ID as the key.
-    plexTokenIdToHypercertId[plexTokenId] = hypercertId;
+        // Store the PLEX token ID in a mapping, with the Hypercert ID as the key.
+        plexTokenIdToHypercertId[plexTokenId] = hypercertId;
 
-    // Verify the data using Hypercerts.
-    require(hypercerts.verifyData(dataHash), "Data verification failed");
+        // Emit an event.
+        emit PlexCertified(plexTokenId, hypercertId);
+    }
 
-    // Associate the verified data with the Plex NFT.
-    tokenData[tokenId] = dataHash;
+    // Event emitted when a PLEX token is certified as a Hypercert.
+    event PlexCertified(uint256 indexed plexTokenId, uint256 indexed hypercertId);
 
-    // Emit an event.
-    emit PlexCertified(tokenId, dataHash);
+    // Events for significant actions.
+    event ClaimMinted(address indexed account, uint256 units, string uri);
+    event ClaimMintedWithFractions(address indexed account, uint256 units, uint256[] fractions, string uri);
+    event FractionSplit(address indexed account, uint256 tokenID, uint256[] values);
+    event FractionsMerged(address indexed account, uint256[] tokenIDs);
+    event FractionBurned(address indexed account, uint256 tokenID);
 }
